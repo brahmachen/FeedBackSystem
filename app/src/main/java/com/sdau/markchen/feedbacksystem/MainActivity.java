bd05fbc9a6ee11aa6977a12468af5a00b853181b
ChenMingcheng <1570789797@qq.com>
2016-01-01 13:27:17 +0800

    增加GPS模块

--- app/src/main/java/com/sdau/markchen/feedbacksystem/MainActivity.java
+++ app/src/main/java/com/sdau/markchen/feedbacksystem/MainActivity.java
@@ -1,9 +1,15 @@
 package com.sdau.markchen.feedbacksystem;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -15,6 +21,7 @@ import com.sdau.markchen.feedbacksystem.R;
 public class MainActivity extends AppCompatActivity {
 
     private int type;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,8 +39,8 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void onClick(View view){
-        switch (view.getId()){
+    public void onClick(View view) {
+        switch (view.getId()) {
             case R.id.id_button_fb1:
                 this.type = 1;
                 break;
@@ -49,12 +56,52 @@ public class MainActivity extends AppCompatActivity {
             case R.id.id_button_fb5:
                 this.type = 5;
                 break;
-            default:break;
+            default:
+                break;
         }
-        Intent intent = new Intent(this,FeedBackActivity.class);
-        intent.putExtra("Type",type);
-        startActivity(intent);
+
+        if (!((LocationManager) getSystemService(Context.LOCATION_SERVICE)).isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(
+                    MainActivity.this);
+            builder.setTitle("提示");
+            builder.setMessage("GPS未开启");
+            builder.setPositiveButton("现在开启",
+                    new AlertDialog.OnClickListener() {
+
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                            startActivityForResult(intent, 0);
+                        }
+                    });
+            builder.setNegativeButton("暂不开启",
+                    new AlertDialog.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            Intent intent = new Intent(MainActivity.this, FeedBackActivity.class);
+                            intent.putExtra("Type", type);
+                            startActivity(intent);
+                        }
+                    });
+            builder.show();
+        } else {
+            Intent intent = new Intent(MainActivity.this, FeedBackActivity.class);
+            intent.putExtra("Type", type);
+            startActivity(intent);
+        }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0 && resultCode == RESULT_OK) {
+            Intent intent = new Intent(MainActivity.this, FeedBackActivity.class);
+            intent.putExtra("Type", type);
+            startActivity(intent);
+        }
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -75,5 +122,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+
     }
 }
